c7078c13509beea97d2c828556ae890146f70065
Gatteber <Garrett.Atteberry@gmail.com>
2023-05-25 10:23:00 +0900

    add api backend

---
+++ server/api/components/projectList.js
@@ -0,0 +1,40 @@
+const projects = [
+  {
+    projectID: 1,
+    projectName: 'Project 1',
+    gitLink: '#',
+    liveLink: '#',
+    image: '#',
+    description:
+      'lorem ipsum color dolor set here is the test description. Additional data can be found here and I will continue to add data so there is enough to make sure I have good coverage. Overall, I think this was a great success of a project.',
+  },
+  {
+    projectID: 2,
+    projectName: 'Project 2',
+    gitLink: '#',
+    liveLink: '#',
+    image: '#',
+    description:
+      'lorem ipsum color dolor set here is the test description. Additional data can be found here and I will continue to add data so there is enough to make sure I have good coverage. Overall, I think this was a great success of a project.',
+  },
+  {
+    projectID: 3,
+    projectName: 'Project 3',
+    gitLink: '#',
+    liveLink: '#',
+    image: '#',
+    description:
+      'lorem ipsum color dolor set here is the test description. Additional data can be found here and I will continue to add data so there is enough to make sure I have good coverage. Overall, I think this was a great success of a project.',
+  },
+  {
+    projectID: 4,
+    projectName: 'Project 4',
+    gitLink: '#',
+    liveLink: '#',
+    image: '#',
+    description:
+      'lorem ipsum color dolor set here is the test description. Additional data can be found here and I will continue to add data so there is enough to make sure I have good coverage. Overall, I think this was a great success of a project. did it update?',
+  },
+];
+
+module.exports = projects;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const projectList = require('./api/components/projectList');
 
-app.get('/api/v1', (req, res) => {
-  res.send({name: 'up and running!'});
+// app.get('/api/v1', (req, res) => {
+//   res.send({name: 'up and running!'});
+// });
+
+app.get('/api/project_list', (req, res) => {
+  res.send(projectList);
 });
 
 const PORT = process.env.PORT || 5000;
